c2f194e4c3cf807e457a37661d518b6f3cd71f16
yoonsang lee <highp0912@pm.me>
2024-11-19 23:10:06 +0900

    add: coffee service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,35 +10,45 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
   @Get()
   @HttpCode(HttpStatus.OK)
-  findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. limit: ${limit}, offset: ${offset}`;
+  findAll(): readonly Coffee[] {
+    return this.coffeeService.findAll();
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAllWithPaginationQuery(@Query() paginationQuery): readonly Coffee[] {
+    return this.coffeeService.findAllWithPagination(paginationQuery);
   }
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+    return this.coffeeService.findOne(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() body) {
-    return body;
+    return this.coffeeService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    console.log('patch: ' + body);
+    return this.coffeeService.update(id, body);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return `This action deletes #${id} coffee`;
+    return this.coffeeService.delete(id);
   }
 }
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,97 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private readonly coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+    {
+      id: 2,
+      name: 'Cheeky Breeky Ale',
+      brand: 'Buddy Brew',
+      flavors: ['blood', 'sweat'],
+    },
+    {
+      id: 3,
+      name: 'K-lager',
+      brand: 'Buddy Brew',
+      flavors: ['piss', 'piss'],
+    },
+    {
+      id: 4,
+      name: 'Limonaed',
+      brand: 'Orchidman',
+      flavors: ['sweat', 'water', 'lemon zest'],
+    },
+    {
+      id: 5,
+      name: 'Cola Coca',
+      brand: 'CC',
+      flavors: ['pot', 'water', 'sugar (not BTS)'],
+    },
+  ];
+
+  findAll(): readonly Coffee[] {
+    return this.coffees;
+  }
+
+  findAllWithPagination(paginationQuery: any): readonly Coffee[] {
+    let { limit, offset } = paginationQuery;
+
+    offset = +offset;
+    limit = +limit;
+
+    // 2 contents at page 1
+    const start = (offset - 1) * limit;
+    const end = start + limit;
+
+    return this.coffees.slice(start, end);
+  }
+
+  findOne(id: string): Coffee | null {
+    const idAsNum = Number.parseInt(id);
+    if (!Number.isInteger(idAsNum)) return null;
+
+    const coffee = this.coffees.find((e) => e.id === idAsNum);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+  }
+
+  update(id: string, updateDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      const idAsNum = Number.parseInt(id);
+      const updateIdx = this.coffees.findIndex((e) => e.id === idAsNum);
+
+      this.coffees[updateIdx] = { ...updateDto };
+    }
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  delete(id: string) {
+    const coffeeIndex = this.coffees.findIndex(
+      (c) => c.id === Number.parseInt(id),
+    );
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+
+  private isCoffeeExisting(id: string): boolean {
+    return this.coffees.findIndex((e) => e.id === +id) !== -1;
+  }
+}
